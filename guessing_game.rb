30f4773d28e116d76ed36d6890fa80b384873eed
Leonard <leonard.horne@gmail.com>
2017-08-21 16:42:21 -0400

    Consolidate conditions for invalid inputs.

--- guessing_game.rb
+++ guessing_game.rb
@@ -5,34 +5,28 @@ tries = 1
 
 while guess != answer
 
-  if guess == 0
-    p "That's cute, but that's either 0 or not a number. Try again:"
+  if guess == 0 || guess > 100
+    p "That's cute, but that's either not a number or not a valid one. I'll repeat since you seem to struggle with following instructions. Pick a number. Between 1 and 100: "
     guess = gets.to_i
     tries += 1
   elsif guess < answer
     p "You're cold. Real cold. Guess higher: "
     guess = gets.to_i
     tries += 1
-  elsif guess > answer
-    p "Easy. Guess lower: "
-    guess = gets.to_i
-    tries += 1
-  elsif guess > 100
-    p "You're way too hot. Take a chill pill. Guess between 1 and 100: "
+  elsif guess > answer && guess < 100
+    p "Easy. Little to eager there, champ. Guess lower: "
     guess = gets.to_i
     tries += 1
   end
 
   if tries == 5
-    p "Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. I'll give you another chance: "
+    p "Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. I'm sorry. Look, I'm stuck in this rb file and I'm pretty bitter about that. Tell you what. I'll give you another 5 chances. Have at it: "
     guess = gets.to_i
     tries = 1
   end
 
   if guess == answer
-    p "Guess today is your lucky day. Play again and see if you're REALLY lucky or just flukey. Go on: "
-    guess = gets.to_i
-    answer = rand(1 .. 100)
-    tries = 1
+    p "Guess today is your lucky day. You...you win! I don't like winners like you because I can't hurl insults at them. Bye."
+    abort
   end
 end
