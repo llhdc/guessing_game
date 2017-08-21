786d2217a8a98d103b374fbafd51794015776e7a
Leonard <leonard.horne@gmail.com>
2017-08-21 15:34:28 -0400

    Add conditions while user guess is correct or incorrect under 5 tries.

---
+++ guessing_game.rb
@@ -0,0 +1,35 @@
+p "Guess a number between 1 and 100: "
+guess = gets.to_i
+number = rand(1 .. 100)
+tries = 1
+
+  while guess != number
+
+    if guess.to_s == true
+      p "That's cute, but that's not a number. Try again."
+      tries += 1
+    elsif guess < number
+      p "You're cold. Real cold. Guess again: "
+      guess = gets.to_i
+      tries += 1
+    elsif guess > number
+      p "You're way too hot. Take a chill pill. Try again: "
+      guess = gets.to_i
+      tries += 1
+    end
+    
+  end
+
+  if tries == 5
+    p "Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. I'll give you another chance: "
+    guess = gets.to_i
+    tries = 0
+  end
+
+  if guess == number
+    p "Guess today is your lucky day. Play again and see if you're REALLY lucky or just flukey. Go on: "
+    guess = gets.to_i
+    tries = 0
+  end
+
+end
