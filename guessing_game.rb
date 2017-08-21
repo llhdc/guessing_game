838ab33b8b02614299dedb0bcbfb062199e0df53
Leonard <leonard.horne@gmail.com>
2017-08-21 15:42:07 -0400

    Correct tries counter.

--- guessing_game.rb
+++ guessing_game.rb
@@ -21,12 +21,12 @@ while guess != answer
   if tries == 5
     p "Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. I'll give you another chance: "
     guess = gets.to_i
-    tries = 0
+    tries = 1
   end
 
   if guess == answer
     p "Guess today is your lucky day. Play again and see if you're REALLY lucky or just flukey. Go on: "
     guess = gets.to_i
-    tries = 0
+    tries = 1
   end
 end
