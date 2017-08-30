d8763a8fbdce3767637012601626d319622cf757
Leonard <leonard.horne@gmail.com>
2017-08-30 09:42:05 -0400

    Fix game.

--- guessing_game.rb
+++ guessing_game.rb
@@ -7,10 +7,10 @@ def get_guess
 end
 
 puts "Guess a number between 1 and 100. You have 5 chances to get it right. Don't mess this up: "
+answer = rand(1 .. 100)
 guess = get_guess
 guesses = []
 guesses.push(guess)
-answer = rand(1 .. 100)
 tries = 1
 
 
@@ -32,25 +32,25 @@ while guess != answer
     tries += 1
   end
 
-  if tries == 5
-    puts "Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. Sorry not sorry. Feel free to play again...when you grow up..."
-    abort
-  end
-
-  if guess == answer
-    puts "Guess today is your lucky day. You...you win! I don't like winners like you because I can't hurl insults at them. Bye."
-    abort
-  end
-
-  if guess == answer && tries == 1
-    puts "Hmmm...you think you're cool don't you? Got it on the first try. Dumb luck, surely. Play again and see if you're REALLY cool, smart, lucky and successful in life. I dare ya."
-    abort
-  end
-
   if guesses.include?(guess)
     puts "You already guessed that."
   end
 
   guesses.push(guess)
 
+end
+
+if tries == 5
+  puts "Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. Sorry not sorry. Feel free to play again...when you grow up..."
+  abort
+end
+
+if guess == answer && tries > 1
+  puts "Guess today is your lucky day. You...you win! I don't like winners like you because I can't hurl insults at them. Bye."
+  abort
+end
+
+if guess == answer && tries == 1
+  puts "Hmmm...you think you're cool don't you? Got it on the first try. Dumb luck, surely. Play again and see if you're REALLY cool, smart, lucky and successful in life. I dare ya."
+  abort
 end
