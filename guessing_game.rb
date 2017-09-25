277fe90f015032a65ee300c2b5c08f797ab92e15
Leonard <leonard.horne@gmail.com>
2017-09-25 14:43:43 -0400

    Ship it!

--- guessing_game.rb
+++ guessing_game.rb
@@ -8,7 +8,6 @@ end
 
 puts "Guess a number between 1 and 100. You have 5 chances to get it right. Don't mess this up: "
 answer = rand(1 .. 100)
-puts answer
 guess = get_guess
 guesses = []
 guesses.push(guess)
@@ -31,18 +30,17 @@ while guess != answer && tries < 5
   end
   guesses.push(guess)
   tries += 1
-  
+
 end
 
-if guess != answer && tries == 5
-  puts "The answer was #{answer}. Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. Sorry not sorry. Feel free to play again...when you grow up..."
-  exit
-elsif guess == answer && tries == 5
-  puts "Got it on the last guess? You're not clutch, you're lucky! Play again to find out what you're REALLY made of."
-elsif guess == answer && tries == 1
-  puts "Hmmm...you think you're cool don't you? Got it on the first try. Dumb luck, surely. Play again and see if you're REALLY cool, smart, lucky and successful in life. I dare ya."
-  exit
+if guess == answer
+  if tries == 5
+    puts "Got it on the last guess? You're not clutch, you're lucky! Play again to find out what you're REALLY made of."
+  elsif tries == 1
+    puts "Hmmm...you think you're cool don't you? Got it on the first try. Dumb luck, surely. Play again and see if you're REALLY cool, smart, lucky and successful in life. I dare ya."
+  else
+    puts "Guess today is your lucky day. You...you win! I don't like winners like you because I can't hurl insults at them. Bye."
+  end
 else
-  puts "Guess today is your lucky day. You...you win! I don't like winners like you because I can't hurl insults at them. Bye."
-  exit
+  puts "The answer was #{answer}. Sorry. Game over. You couldn't figure it out, even after 5 tries. You're clearly not very good at this. Oh, come on, don't cry about it. Sorry not sorry. Feel free to play again...when you grow up..."
 end
